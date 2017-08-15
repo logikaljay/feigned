5aa88beb0bcf96fe2ff73194870bd594ed75be3d
Jay Baker <logikal@gmail.com>
2017-08-16 08:28:40 +1200

    feat(CLI): updating CLI

--- bin/helpers.js
+++ bin/helpers.js
@@ -1,4 +1,16 @@
+const { spawn } = require('child_process')
 module.exports = {
+  start: (bin, args) => {
+    const proc = spawn(bin, args, { stdio: 'inherit', customFds: [0, 1, 2] })
+    proc.on('close', code => process.exit(code))
+    proc.on('error', err => {
+      console.error(err)
+      process.exit(1)
+    })
+
+    return proc
+  },
+
   validate: function(routes) {
     var errors = []
 
